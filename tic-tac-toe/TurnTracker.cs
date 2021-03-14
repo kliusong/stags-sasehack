5d5a5ba64483b95bb77bbecdf8d01d65753df505
kliusong <80644101+kliusong@users.noreply.github.com>
2021-03-14 14:23:59 -0500

    Add files via upload

---
+++ tic-tac-toe/TurnTracker.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTracker : MonoBehaviour
+{
+    public bool xTurn = true;
+
+
+
+/// private GameObject a;
+/// private GameObject b;
+/// private GameObject c;
+/// private GameObject d;
+/// private GameObject e;
+/// private GameObject f;
+/// private GameObject g;
+/// private GameObject h;
+/// void Start()
+///    
+/// Invoke("Work", 5f);
+
+
+  ///  private void Work()
+    ///
+   ///     a = Instantiate(GameObject.Find("Board_Button.glb"), GameObject.Find("Board_Button.glb").transform.position + new Vector3(170f - 1138f, 0, 0), Quaternion.identity);
+ ///      b = Instantiate(GameObject.Find("Board_Button.glb"), GameObject.Find("Board_Button.glb").transform.position + new Vector3(-170f - 1138f, 0, 0), Quaternion.identity);
+  ///      c = Instantiate(GameObject.Find("Board_Button.glb"), GameObject.Find("Board_Button.glb").transform.position + new Vector3(170f - 1138f, 0, 170f), Quaternion.identity);
+  ////     d = Instantiate(GameObject.Find("Board_Button.glb"), GameObject.Find("Board_Button.glb").transform.position + new Vector3(-170f - 1138f, 0, -170f), Quaternion.identity);
+ ///     e = Instantiate(GameObject.Find("Board_Button.glb"), GameObject.Find("Board_Button.glb").transform.position + new Vector3(170f - 1138f, 0, -170f), Quaternion.identity);
+   ///    f = Instantiate(GameObject.Find("Board_Button.glb"), GameObject.Find("Board_Button.glb").transform.position + new Vector3(-170f - 1138f, 0, 170f), Quaternion.identity);
+  ///     g = Instantiate(GameObject.Find("Board_Button.glb"), GameObject.Find("Board_Button.glb").transform.position + new Vector3(0 - 1138f, 0, 170f), Quaternion.identity);
+   ///     h = Instantiate(GameObject.Find("Board_Button.glb"), GameObject.Find("Board_Button.glb").transform.position + new Vector3(0 - 1138f, 0, -170f), Quaternion.identity);
+///
+   ///     Debug.Log("Fuck!"); 
+}
